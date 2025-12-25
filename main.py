a7c4fe1821cfc9bdd41d6e83e2727a7ce59ad4da
ayvkma <ayushv1347@gmail.com>
2025-12-25 15:04:20 +0530

    Finalize v1: in-memory key-value store with TTL and snapshots

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import copy
 import json
 import time
+import uuid
 import threading
 from datetime import datetime, timedelta
 from pathlib import Path
 
-CLEAN_UP_INTERVAL_IN_SECONDS = 5
+CLEAN_UP_INTERVAL_IN_SECONDS = 60
 EXPIRATION_TIME_IN_SECONDS = 10
 FILE = Path('snapshots.log')
 STORE = {}
@@ -19,7 +20,14 @@ def convert_datetime_to_iso(store):
             store[key]['expire_at'] = expire_at_iso
     return store
 
-# def load_data():
+def convert_iso_to_datetime(store):
+    for key in store:
+        if not store[key]['expire_at']:
+            store[key]['expire_at'] = None
+        else:
+            expire_at_iso = datetime.fromisoformat(store[key]['expire_at'])
+            store[key]['expire_at'] = expire_at_iso
+    return store
     
 def clean_up():
     while True:
@@ -29,16 +37,37 @@ def clean_up():
         for key in keys:
             if STORE[key]['expire_at'] and STORE[key]['expire_at'] <= datetime.now():
                 del STORE[key]
-    
-        store = convert_datetime_to_iso(store=copy.deepcopy(STORE))
-        with open(FILE, 'a') as file:
-            file.write(json.dumps(store) + '\n')
+
+        try:
+            if STORE != {}:
+                store = convert_datetime_to_iso(store=copy.deepcopy(STORE))
+                lines = None
+                with open(FILE, 'r') as file:
+                    lines = file.readlines()
+                new_file = Path(str(uuid.uuid4()) + '.log')
+                with open(new_file, 'w') as file:
+                    for line in lines:
+                        file.write(line)
+                    file.write(json.dumps(store) + '\n')
+                new_file.replace(FILE)
+
+        except Exception as e:
+            print('Error: ', e)
 
         time.sleep(CLEAN_UP_INTERVAL_IN_SECONDS)
         
-
-# TODO - LOADING PERSISTED DATA ON LOAD
-                    
+def load_data(FILE) -> dict:
+    with open(FILE, '+a') as file:
+        # Move the file pointer to the beginning to read all contents
+        file.seek(0)
+        lines = file.readlines()
+        if len(lines) < 1:
+            return {}
+        last_recorded_snapshot = lines[-1].strip('\n\r')
+        store = json.loads(last_recorded_snapshot)
+        store = convert_iso_to_datetime(store)
+        return store
+    
 def main():
     thread = threading.Thread(target=clean_up, daemon=True)
     thread.start()
@@ -85,6 +114,8 @@ def main():
             break          
         else:
             print(f'{command} command is not supported.')
-        
+            
 if __name__ == '__main__':
+    # Load on start
+    STORE = load_data(FILE)
     main()
